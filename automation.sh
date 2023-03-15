19ab12681a7ba9962c5ac02c8360030075fdbaf5
Parikshit Hiwase <55256204+Parikshit001@users.noreply.github.com>
2023-03-15 23:50:35 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -24,10 +24,26 @@ timestamp=$(date '+%d%m%Y-%H%M%S')
 cd /var/log/apache2
 sudo tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
 
+s3_bucket=”upgradparikshit”
+
 if [[ -f/tmp/${name}-httpd-logs-${timestamp}.tar ]]
 then
 	aws s3 \
-	cp /tmp/${myname}-httpd-logs-${timestamp}.tar \
-	s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+	cp /tmp/${name}-httpd-logs-${timestamp}.tar \
+	s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 fi
 
+
+docroot="/var/www/html"
+if [[ ! -f ${docroot}/inventory.html ]]; then
+	echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' >  ${docroot}/inventory.html
+fi
+
+if [[ -f ${docroot}/inventory.html ]]; then
+	size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+	echo -e \n"httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]; then
+	echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
+fi
